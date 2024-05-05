20048877a93c18996d993f94c19140ca59dc5f8e
lvhills <144601663+lvhills@users.noreply.github.com>
2024-05-05 18:03:39 +0700

    NO 2 DONE

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,6 +1,8 @@
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
 
+let loginAttempts = {};
+
 /**
  * Handle login request
  * @param {object} request - Express request object
@@ -12,6 +14,29 @@ async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
+    if (loginAttempts[email]) {
+      const lastAttempt = loginAttempts[email];
+      const timeelapsed =
+        (new Date() - lastAttempt.lastAttemptTime) / (1000 * 60);
+      if (timeelapsed >= 30) {
+        loginAttempts[email] = {
+          failedAttempts: 0,
+          lastAttemptTime: new Date(),
+        };
+      }
+    } else {
+      loginAttempts[email] = { failedAttempts: 0, lastAttemptTime: new Date() };
+    }
+
+    const limitLogin = loginAttempts[email].failedAttempts >= 5;
+
+    if (limitLogin) {
+      throw errorResponder(
+        errorTypes.FORBIDDEN,
+        'Too many failed login attempts. Please try again in 30 minutes'
+      );
+    }
+
     // Check login credentials
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
@@ -19,11 +44,14 @@ async function login(request, response, next) {
     );
 
     if (!loginSuccess) {
+      loginAttempts[email].failedAttempts++;
       throw errorResponder(
         errorTypes.INVALID_CREDENTIALS,
-        'Wrong email or password'
+        `Wrong email or password, ${5 - loginAttempts[email].failedAttempts} chances left`
       );
     }
+
+    loginAttempts[email] = { failedAttempts: 0, lastAttemptTime: new Date() };
 
     return response.status(200).json(loginSuccess);
   } catch (error) {
